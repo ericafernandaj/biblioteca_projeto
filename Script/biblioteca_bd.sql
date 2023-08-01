a8b6cfe751475ca1308c61a1e2da882cbccaf012
ericafernandaj <eriicafernandaj@gmail.com>
2023-08-01 17:02:33 -0300

    Adicionado Comentários

--- Script/biblioteca_bd.sql
+++ Script/biblioteca_bd.sql
@@ -1,6 +1,6 @@
 create database Biblioteca;
 use Biblioteca;
-
+-- Criação da tabela Cliente
 CREATE TABLE `Cliente` (
   `idcliente` int PRIMARY KEY,
   `nome_cliente` varchar(255),
@@ -10,7 +10,7 @@ CREATE TABLE `Cliente` (
   `endereco` varchar(255),
   `recomendacao` varchar(255)
 );
-
+-- Criação da tabela Livro
 CREATE TABLE `Livro` (
   `id_livro` int PRIMARY KEY,
   `nome_livro` varchar(255),
@@ -18,7 +18,7 @@ CREATE TABLE `Livro` (
   `ano_publicacao` int,
   `data_devolucao` date
 );
-
+-- Criação da tabela Autor
 CREATE TABLE `Autor` (
   `id_autor` int PRIMARY KEY,
   `nome_autor` varchar(255),
@@ -26,7 +26,7 @@ CREATE TABLE `Autor` (
   `genero` varchar(255),
   `nacionalidade` varchar(255)
 );
-
+-- Criação da tabela Emprestimo
 CREATE TABLE `Emprestimo` (
   `id_emprestimo` int PRIMARY KEY,
   `email` varchar(255),
@@ -34,20 +34,20 @@ CREATE TABLE `Emprestimo` (
   `data_emprestimo` date,
   `id_cliente` int
 );
-
+-- Criação da tabela Editora
 CREATE TABLE `Editora` (
   `id_editora` int PRIMARY KEY,
   `ano_publicacao` int,
   `nome_editora` varchar(255),
   `pais_origem` varchar(255)
 );
-
+-- Adição de chave estrangeira na tabela Emprestimo referenciando a tabela Cliente
 ALTER TABLE `Emprestimo` ADD FOREIGN KEY (`id_cliente`) REFERENCES `Cliente` (`idcliente`);
-
+-- Adição de chave estrangeira na tabela Livro referenciando a tabela Cliente
 ALTER TABLE `Livro` ADD FOREIGN KEY (`recomendacao`) REFERENCES `Cliente` (`recomendacao`);
-
+-- Adição de chave estrangeira na tabela Livro referenciando a tabela Editora
 ALTER TABLE `Livro` ADD FOREIGN KEY (`ano_publicacao`) REFERENCES `Editora` (`ano_publicacao`);
-
+-- Adição de chave estrangeira na tabela Autor referenciando a tabela Livro
 ALTER TABLE `Autor` ADD FOREIGN KEY (`nome_editora`) REFERENCES `Livro` (`nome_livro`);
-
+-- Adição de chave estrangeira na tabela Autor referenciando a tabela Livro
 ALTER TABLE `Autor` ADD FOREIGN KEY (`nome_autor`) REFERENCES `Livro` (`nome_livro`);
--- Script/script_bd.sql
+++ Script/script_bd.sql
@@ -1,3 +1,4 @@
+-- Inserção de registros na tabela "autor"
 INSERT INTO autor (id_autor, nome_autor, nome_editora, genero, nacionalidade) values
 (1,'Antonela França', 'Grupo Planeta', 'Suspense', 'Brasileiro'),
 (2,'Sophia Williams', 'Editora Nacional', 'Romance', 'Americana'),
@@ -9,6 +10,7 @@ INSERT INTO autor (id_autor, nome_autor, nome_editora, genero, nacionalidade) va
 (8,'Isabella da Silva', 'Editora Nova Fronteira', 'Aventura', 'Brasileira'),
 (9,'Ivo Moreira','Ovns e Ciencia','Ciencia','Brasileiro');
 
+-- Inserção de registros na tabela "cliente"
 INSERT INTO cliente (idcliente, nome_cliente, email, telefone, data_nascimento, endereco, recomendacao) values
 (1,'Rosalva de Oliveira','rosal@gmail.com','41998675647','1987-06-23','rua das camelias 34','o beijo do vampiro'),
 (2,'Mateus Ferreira','mateus@gmail.com','41998765432','1995-02-14','av. dos Girassois 56','O Poderoso Chefão'),
@@ -18,6 +20,7 @@ INSERT INTO cliente (idcliente, nome_cliente, email, telefone, data_nascimento, 
 (6,'Lucas Rodrigues','lucas@gmail.com','41997788990','1997-08-10','rua das Margaridas 45','Matrix'),
 (7,'Bianca Souza','bianca@gmail.com','41998877665','1988-04-03','av. dos Lirios 78','Forrest Gump');
 
+-- Inserção de registros na tabela "editora"
 INSERT INTO	editora (id_editora, ano_publicacao, nome_editora, pais_origem) values
 (1,1998,'Grupo Planeta','Brasil'),
 (2,1996,'Editora Nacional','Brasil'),
@@ -29,6 +32,7 @@ INSERT INTO	editora (id_editora, ano_publicacao, nome_editora, pais_origem) valu
 (8,2015,'Editora Nova','Brasil'),
 (9,2003,'Ovns e Ciencia','Brasil');
 
+-- Inserção de registros na tabela "emprestimo"
 INSERT INTO emprestimo (id_emprestimo, email, data_devolucao, data_emprestimo, id_cliente) values
 (1,'rosal@gmail.com','2020-07-01','2020-06-28',null),
 (2,'mateus@gmail.com','2021-08-10','2021-07-25',null),
@@ -38,6 +42,7 @@ INSERT INTO emprestimo (id_emprestimo, email, data_devolucao, data_emprestimo, i
 (6,'lucas@gmail.com','2020-07-01','2020-06-28',null),
 (7,'bianca@gmail.com','2020-07-11','2020-06-28',null);
 
+-- Inserção de dados na tabela "livro"
 INSERT INTO livro (id_livro, nome_livro, recomendacao, ano_publicacao, data_devolucao) values
 (1, 'O Homem do Labirinto','o beijo do vampiro',1987,'2020-07-01'),
 (2, 'A Garota no Trem','O Poderoso Chefão',1995,'2021-08-10'),
